f4920aeab17e64911d4ec598d62702da07ba3d1c
DiegoV <ddvallejoj@gmail.com>
2024-07-02 19:55:25 -0600

    'pngs'

--- app.py
+++ app.py
@@ -24,16 +24,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 def titlesfromwebsite(url, htmltag, doctitle):
     getter = requests.get(url)
-    soup = BeautifulSoup(getter.content, 'html.parser')
+    soup = BeautifulSoup(getter.content, "html.parser")
     titles = [i.text for i in soup.find_all(htmltag)]
-    txtstring = ''
-    for i in titles: 
-        txtstring += (f"{i}\n")
-    with open (f"{doctitle}.txt", "w", encoding="utf-8") as file: 
+    txtstring = ""
+    for i in titles:
+        txtstring += f"{i}\n"
+    with open(f"{doctitle}.txt", "w", encoding="utf-8") as file:
         file.write(txtstring)
     return txtstring
+
 
 # @app.get("/webscrapping/{link}/{tag}/{name}")
 # def webscrapping(link, tag, name):
@@ -42,30 +44,35 @@ def titlesfromwebsite(url, htmltag, doctitle):
 #     titles = [i.text for i in soup.find_all(tag)]
 #     return {"website": name, "urls": titles}
 
+
 @app.get("/webscrapping/{link}/{tag}/{name}")
 def webscrapping(link, tag, name):
     getter = requests.get(link.replace("-", "/"))
-    soup = BeautifulSoup(getter.content, 'html.parser')
-    
+    soup = BeautifulSoup(getter.content, "html.parser")
+
     article_urls = []
     for a in soup.find_all(tag):
-        hr = a.find('a')
+        hr = a.find("a")
         # print(hr['href'])
-        article_urls.append(hr['href'])
+        article_urls.append(hr["href"])
     # article_urls = [a['href'] for a in soup.find_all(tag, href=True)]
     articles = []
     for url in article_urls:
         article_getter = requests.get(url)
-        article_soup = BeautifulSoup(article_getter.content, 'html.parser')
+        article_soup = BeautifulSoup(article_getter.content, "html.parser")
         try:
             article_title = article_soup.find("h1", {"class": "a_t"}).text
             article_content = article_soup.find("h2", {"class": "a_st"}).text
             # article_content = article_soup.find('div', {'class': 'a_c clearfix'}).text
-            articles.append({"url": url, "title": article_title, "content": article_content})
+            print({"url": url, "title": article_title, "content": article_content})
+            articles.append(
+                {"url": url, "title": article_title, "content": article_content}
+            )
         except:
             pass
     news_collection.insert_one({"website": name, "articles": articles})
     return {"website": name, "articles": articles}
+
 
 # titlesfromwebsite("https:--elpais.com-america-", "h2", "El Pais")
 # titlesfromwebsite("https://www.prensalibre.com/", "h1", "Prensa Libre")
@@ -74,6 +81,4 @@ def webscrapping(link, tag, name):
 # titlesfromwebsite("https://www.elmundo.es/", "h2", "El Mundo")
 
 if __name__ == "__main__":
-    uvicorn.run(app="app :app", host="0.0.0.0", port=8000,
-                reload=True
-                )
+    uvicorn.run(app="app :app", host="0.0.0.0", port=8000, reload=True)
